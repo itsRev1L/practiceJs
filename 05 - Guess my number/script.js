790e28f09af15e6c608f64f7734cb82094910da5
itsRev1L <itsmoody.ldbl@gmail.com>
2024-03-25 21:13:54 +0330

    v1.5
    cleaning the code.
    i'm not sure using function for this type of coding is good but i did it anyway

--- 05 - Guess my number/script.js
+++ 05 - Guess my number/script.js
@@ -2,6 +2,7 @@
 let guessNumber = Math.trunc(Math.random() * 20 + 1);
 let gameScore = 20;
 let highScore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const inputGuess = Number(document.querySelector(".guess").value);
   //this is for when the number is higher than the random number
@@ -9,49 +10,33 @@ document.querySelector(".check").addEventListener("click", function () {
   if (inputGuess === guessNumber) {
     //css back ground style
     document.querySelector("body").style.backgroundColor = "#03a400";
-    document.querySelector(".message").textContent = "Correct";
+    displayMessage("message", "correct");
     //highScore
     if (gameScore > highScore) {
       highScore = gameScore;
       document.querySelector(".Highscore").textContent = highScore;
     }
-    //if number is higher than 0
-  } else if (inputGuess > guessNumber) {
+  } else if (inputGuess !== guessNumber) {
     if (gameScore > 0) {
-      //and if number is equal to 1 do this
-      if (gameScore == 1) {
-        document.querySelector(".message").textContent = "game over";
-        document.querySelector(".score").textContent = 0;
-      } else {
-        // and if its not 0 or less
-        document.querySelector(".message").textContent = "Too high ";
-        gameScore--;
-        document.querySelector(".score").textContent = gameScore;
-      }
-    }
-    //this is for when the number is lower than the random number
-  } else if (inputGuess < guessNumber) {
-    //if number is higher than 0
-    if (gameScore > 0) {
-      //and if number is equal to 1 do this
-      if (gameScore == 1) {
-        document.querySelector(".message").textContent = "game over ";
-        document.querySelector(".score").textContent = 0;
-      } else {
-        // and if its not 0 or less
-        document.querySelector(".message").textContent = "Too low ";
-        gameScore--;
-
-        document.querySelector(".score").textContent = gameScore;
-      }
+      // if Number is higher than the random number then the game will say its too high other wise it says its too low
+      document.querySelector(".message").textContent = inputGuess > guessNumber ? "Too high" : "too low";
+      gameScore--;
+      displayMessage("score", gameScore);
+    } else {
+      displayMessage("message", "game over");
+      displayMessage("score", 0);
     }
   }
 });
 document.querySelector(".again").addEventListener("click", function () {
   gameScore = 20;
   guessNumber = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector(".score").textContent = gameScore;
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("score", gameScore);
+  displayMessage("message", "Start guessing...");
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
 });
+
+function displayMessage(cl, ms) {
+  document.querySelector(`.${cl}`).textContent = `${ms}`;
+}
