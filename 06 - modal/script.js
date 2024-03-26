c3ba49e173060126adfcaff10dfc7f7bb56812b9
itsRev1L <itsmoody.ldbl@gmail.com>
2024-03-27 03:14:03 +0330

    first commit

---
+++ 06 - modal/script.js
@@ -0,0 +1,22 @@
+//variables
+const showModalBtn = document.querySelectorAll('.show-modal');
+const hiddenModal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
+const backGround = document.querySelector('.overlay');
+//functions
+const closeSocial = function () {
+  hiddenModal.classList.add('hidden');
+  backGround.classList.add('hidden');
+};
+
+const openSocial = function () {
+  hiddenModal.classList.remove('hidden');
+  backGround.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', openSocial);
+}
+
+closeBtn.addEventListener('click', closeSocial);
+backGround.addEventListener('click', closeSocial);
