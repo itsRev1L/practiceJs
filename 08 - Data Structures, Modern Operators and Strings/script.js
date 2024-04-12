e8b81ded5e01049aed19711f6beef8e86c5237a3
itsRev1L <itsmoody.ldbl@gmail.com>
2024-04-12 21:11:54 +0330

    v1.1

--- 08 - Data Structures, Modern Operators and Strings/script.js
+++ 08 - Data Structures, Modern Operators and Strings/script.js
@@ -11,32 +11,43 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //    },
-  // },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
   Test: function (main, sec) {
     return [this.categories[main], this.categories[sec]];
   },
 };
+//destructing arrays
+// let [x, y] = restaurant.mainMenu;
 
-let [x, y] = restaurant.mainMenu;
+// console.log(x, y);
 
-console.log(x, y);
+// [x, y] = [y, x];
+// console.log(x, y);
 
-[x, y] = [y, x];
-console.log(x, y);
+// console.log(restaurant.Test(0, 2));
+// console.log();
 
-console.log(restaurant.Test(0, 2));
-console.log();
+let { openingHours: A } = restaurant;
+console.log(A);
+
+let a = 202;
+let b = 110;
+const obj = { a: 120, b: 20, c: 300 };
+
+({ a, b } = obj);
+
+console.log(a, b);
